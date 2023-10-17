10cb4c145913cbc5d2dd5b990377f7553c6c04dc
Wolfgang Weh <wolfgang.weh@neanex.com>
2023-10-17 17:07:35 +0200

    Review mass units

--- mass_units.go
+++ mass_units.go
@@ -28,11 +28,14 @@ var (
 	Ounce  = newUnit("ounce", "oz", Mass, BI)
 	Pound  = newUnit("pound", "lb", Mass, BI)
 	Stone  = newUnit("stone", "st", Mass, BI)
-	Ton    = newUnit("ton", "t", Mass, BI)
+	Ton    = newUnit("ton", "LT", Mass, BI)
 	Slug   = newUnit("slug", "", Mass, BI)
 )
 
 func init() {
+	MegaGram.AddAliases("tonne", "tonnes", "metric ton", "metric tons")
+	MegaGram.AddAliases("t")
+
 	NewRatioConversion(Grain, Gram, 0.06479891)
 	NewRatioConversion(Drachm, Gram, 1.7718451953125)
 	NewRatioConversion(Ounce, Gram, 28.349523125)
@@ -40,6 +43,9 @@ func init() {
 	NewRatioConversion(Stone, Gram, 6350.29318)
 
 	// 1 long ton
+	// https://en.wikipedia.org/wiki/Ton
+	// https://en.wikipedia.org/wiki/Long_ton
 	NewRatioConversion(Ton, Gram, 1016046.9088)
+	Ton.AddAliases("long ton", "long tons", "imperial ton", "displacement ton")
 	NewRatioConversion(Slug, Gram, 14593.90294)
 }
