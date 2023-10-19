946e03b72dd80424719b3121f621842a3609d189
Wolfgang Weh <wolfgang.weh@neanex.com>
2023-10-19 13:22:44 +0200

    Add angle units

---
+++ angle_units.go
@@ -0,0 +1,48 @@
+package units
+
+import "math"
+
+var (
+	Angle = UnitOptionQuantity("angle")
+
+	Turn = newUnit("turn", "tr", Angle)
+
+	Radian      = newUnit("radian", "rad", Angle, SI)
+	MilliRadian = Milli(Radian)
+
+	// Degree (= decimal degree) is a unit of angle equal to 1/360 of a circle.
+	Degree = newUnit("degree", "°", Angle)
+
+	Gon      = newUnit("gon", "gon", Angle)
+	DeciGon  = Deci(Gon)
+	CentiGon = Centi(Gon)
+	MilliGon = Milli(Gon)
+	MicroGon = Micro(Gon)
+	NanoGon  = Nano(Gon)
+	PicoGon  = Pico(Gon)
+	FemtoGon = Femto(Gon)
+	AttoGon  = Atto(Gon)
+	ZeptoGon = Zepto(Gon)
+	YoctoGon = Yocto(Gon)
+	DecaGon  = Deca(Gon)
+	HectoGon = Hecto(Gon)
+	KiloGon  = Kilo(Gon)
+	MegaGon  = Mega(Gon)
+	GigaGon  = Giga(Gon)
+	TeraGon  = Tera(Gon)
+	PetaGon  = Peta(Gon)
+)
+
+func init() {
+	NewRatioConversion(Turn, Radian, 2*math.Pi)
+	NewRatioConversion(Turn, Degree, 360)
+	NewRatioConversion(Turn, Gon, 400)
+
+	Gon.AddAliases("gradian", "gradians", "grads")
+	Gon.AddSymbols("grad")
+
+	Degree.AddAliases("degrees", "degree of arc", "degrees of arc", "arc degree", "arcdegree")
+	Degree.AddSymbols("deg")
+
+	Turn.AddSymbols("pla", "rev", "cyc")
+}
---
+++ angle_units_test.go
@@ -0,0 +1,28 @@
+package units
+
+import (
+	"fmt"
+	"math"
+	"testing"
+)
+
+var angleConvTests = []conversionTest{
+	{"turn", "degree", "360"},
+	{"turn", "gon", "400"},
+	{"turn", "radian", fmt.Sprintf("%f", 2*math.Pi)},
+	{"turn", "milliradian", fmt.Sprintf("%f", 2*math.Pi*1000)},
+	{"radian", "milliradian", "1000"},
+	{"radian", "degree", fmt.Sprintf("%.5f", 180/math.Pi)},
+	{"radian", "gon", fmt.Sprintf("%f", 200/math.Pi)},
+	{"degree", "gon", fmt.Sprintf("%f", 10/9.0)},
+	{"gon", "degree", "0.9"},
+	{"gon", "milligon", "1000"},
+	{"gon", "centigon", "100"},
+	{"gon", "decigon", "10"},
+	{"gon", "decagon", "0.1"},
+	{"gon", "hectogon", "0.01"},
+}
+
+func Test_AngleConversions(t *testing.T) {
+	testConversions(t, angleConvTests)
+}
