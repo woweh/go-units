6f94362993434aa2d8b49a6e26268e95474a0c62
Wolfgang Weh <wolfgang.weh@neanex.com>
2023-10-19 23:18:02 +0200

    Add density units

---
+++ density_units.go
@@ -0,0 +1,46 @@
+package units
+
+var (
+	Density = UnitOptionQuantity("density")
+
+	// metric
+	GramPerCubicCentimeter     = newUnit("gram per cubic centimeter", "g/cm³", Density, SI)
+	KilogramPerCubicCentimeter = newUnit("kilogram per cubic centimeter", "kg/cm³", Density, SI)
+	GramPerCubicMeter          = newUnit("gram per cubic meter", "g/m³", Density, SI)
+	KilogramPerCubicMeter      = newUnit("kilogram per cubic meter", "kg/m³", Density, SI)
+	GramPerMilliliter          = newUnit("gram per milliliter", "g/mL", Density, SI)
+	GramPerLiter               = newUnit("gram per liter", "g/L", Density, SI)
+	KilogramPerLiter           = newUnit("kilogram per liter", "kg/L", Density, SI)
+
+	// imperial
+	OuncePerCubicInch = newUnit("ounce per cubic inch", "oz/in³", Density, BI)
+	OuncePerCubicFoot = newUnit("ounce per cubic foot", "oz/ft³", Density, BI)
+	OuncePerGallon    = newUnit("ounce per gallon", "oz/gal", Density, BI)
+	PoundPerCubicInch = newUnit("pound per cubic inch", "lb/in³", Density, BI)
+	PoundPerCubicFoot = newUnit("pound per cubic foot", "lb/ft³", Density, BI)
+	PoundPerGallon    = newUnit("pound per gallon", "lb/gal", Density, BI)
+	SlugPerCubicFoot  = newUnit("slug per cubic foot", "slug/ft³", Density, BI)
+	TonPerCubicYard   = newUnit("ton per cubic yard", "l ton/yd³", Density, BI)
+)
+
+func init() {
+	// KilogramPerCubicMeter is the base unit of Density
+
+	// Metric to Metric conversions
+	NewRatioConversion(GramPerCubicCentimeter, KilogramPerCubicMeter, 1000)
+	NewRatioConversion(KilogramPerCubicCentimeter, KilogramPerCubicMeter, 1000000)
+	NewRatioConversion(GramPerCubicMeter, KilogramPerCubicMeter, 0.001)
+	NewRatioConversion(GramPerMilliliter, KilogramPerCubicMeter, 1000)
+	NewRatioConversion(GramPerLiter, KilogramPerCubicMeter, 1)
+	NewRatioConversion(KilogramPerLiter, KilogramPerCubicMeter, 1000)
+
+	// Imperial to Metric conversions
+	NewRatioConversion(OuncePerCubicInch, KilogramPerCubicMeter, 1729.994)
+	NewRatioConversion(OuncePerCubicFoot, KilogramPerCubicMeter, 1.001153)
+	NewRatioConversion(OuncePerGallon, KilogramPerCubicMeter, 6.236023)
+	NewRatioConversion(PoundPerCubicInch, KilogramPerCubicMeter, 27679.90471)
+	NewRatioConversion(PoundPerCubicFoot, KilogramPerCubicMeter, 16.018463)
+	NewRatioConversion(PoundPerGallon, KilogramPerCubicMeter, 99.776372)
+	NewRatioConversion(SlugPerCubicFoot, KilogramPerCubicMeter, 515.3788184)
+	NewRatioConversion(TonPerCubicYard, KilogramPerCubicMeter, 1328.939)
+}
---
+++ density_units_test.go
@@ -0,0 +1,51 @@
+package units
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var densityConvTests = []conversionTest{
+	{"g/cm³", "kg/cm³", "0.001"},
+	{"kg/cm³", "kg/m³", "1000000"},
+	{"g/m³", "kg/m³", "0.001"},
+	{"g/mL", "kg/m³", "1000"},
+	{"g/L", "kg/m³", "1"},
+	{"kg/L", "kg/m³", "1000"},
+	{"oz/in³", "kg/m³", "1729.994"},
+	{"oz/ft³", "kg/m³", "1.001153"},
+	{"oz/gal", "kg/m³", "6.236023"},
+	{"lb/in³", "kg/m³", "27679.90471"},
+	{"lb/ft³", "kg/m³", "16.018463"},
+	{"lb/gal", "kg/m³", "99.776372"},
+	{"slug/ft³", "kg/m³", "515.378818"},
+	{"l ton/yd³", "kg/m³", "1328.939"},
+	{"oz/in³", "l ton/yd³", "1.301786"},
+	{"oz/in³", "slug/ft³", "3.356743"},
+}
+
+func Test_DensityConversions(t *testing.T) {
+	testConversions(t, densityConvTests)
+}
+
+func Test_DensitySystem(t *testing.T) {
+	si := SiSystem
+	assert.Equal(t, si, GramPerCubicCentimeter.System())
+	assert.Equal(t, si, KilogramPerCubicCentimeter.System())
+	assert.Equal(t, si, GramPerCubicMeter.System())
+	assert.Equal(t, si, KilogramPerCubicMeter.System())
+	assert.Equal(t, si, GramPerMilliliter.System())
+	assert.Equal(t, si, GramPerLiter.System())
+	assert.Equal(t, si, KilogramPerLiter.System())
+
+	bi := BiSystem
+	assert.Equal(t, bi, OuncePerCubicInch.System())
+	assert.Equal(t, bi, OuncePerCubicFoot.System())
+	assert.Equal(t, bi, OuncePerGallon.System())
+	assert.Equal(t, bi, PoundPerCubicInch.System())
+	assert.Equal(t, bi, PoundPerCubicFoot.System())
+	assert.Equal(t, bi, PoundPerGallon.System())
+	assert.Equal(t, bi, SlugPerCubicFoot.System())
+	assert.Equal(t, bi, TonPerCubicYard.System())
+}
